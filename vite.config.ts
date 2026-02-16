e06812519d968415a354e30d23a33abd2794c0c6
AmaneSuou <ozsoymurat387@gmail.com>
2026-02-16 22:16:33 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/fuck/',   // ← trailing slash is required here
 })
